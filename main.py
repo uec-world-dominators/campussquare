d687fa0c0fea379ae639b7a0c8bc9eda23f880d5
shosatojp <me@shosato.jp>
2021-10-06 19:21:54 +0900

    uecの場合

--- main.py
+++ main.py
@@ -1,37 +1,49 @@
 import os
-from uecauth.shibboleth import ShibbolethAuthenticator
-from uecauth.password import DefaultPasswordProvider
-from uecauth.mfa import AutoTOTPMFAuthCodeProvider
+import sys
 from campussquare import CampusSquare
 from campussquare.util import get_flow_execution_key
+from campussquare.cli import get_parser, parse_args_with_handler, Authenticator
 
 
-# Shibboleth Login
-campusweb_url = 'https://campusweb.office.uec.ac.jp/campusweb/ssologin.do'
-shibboleth_host = 'shibboleth.cc.uec.ac.jp'
+class UECCampusSquareAuthenticator(Authenticator):
+    def __init__(self) -> None:
+        self.campusweb_do_url = 'https://campusweb.office.uec.ac.jp/campusweb/campussquare.do'
+        self.campusweb_url = 'https://campusweb.office.uec.ac.jp/campusweb/ssologin.do'
 
-shibboleth = ShibbolethAuthenticator(
-    original_url=campusweb_url,
-    shibboleth_host=shibboleth_host,
-    mfa_code_provider=AutoTOTPMFAuthCodeProvider(os.environ['UEC_MFA_SECRET']),
-    password_provider=DefaultPasswordProvider(
-        os.environ['UEC_USERNAME'],
-        os.environ['UEC_PASSWORD']
-    ),
-    debug=False,
-)
+        # Shibboleth Login
+        from uecauth.shibboleth import ShibbolethAuthenticator
+        from uecauth.password import DefaultPasswordProvider
+        from uecauth.mfa import AutoTOTPMFAuthCodeProvider
+        self.shibboleth_host = 'shibboleth.cc.uec.ac.jp'
+        self.shibboleth = ShibbolethAuthenticator(
+            shibboleth_host=self.shibboleth_host,
+            mfa_code_provider=AutoTOTPMFAuthCodeProvider(os.environ['UEC_MFA_SECRET']),
+            password_provider=DefaultPasswordProvider(
+                os.environ['UEC_USERNAME'],
+                os.environ['UEC_PASSWORD']
+            ),
+            debug=False,
+        )
 
-res = shibboleth.login(campusweb_url)
-print(res.url)
+    def login(self) -> CampusSquare:
+        return CampusSquare(
+            self.campusweb_do_url,
+            None,
+            self.shibboleth.get_cookies(),
+            debug=False
+        )
+
+    def refresh(self) -> CampusSquare:
+        print('refreshing...', file=sys.stderr)
+        res = self.shibboleth.login(self.campusweb_url)
+        return CampusSquare(
+            self.campusweb_do_url,
+            get_flow_execution_key(res.url),
+            self.shibboleth.get_cookies(),
+            debug=False
+        )
 
 
-# Campus Square
-campussquare = CampusSquare(
-    'https://campusweb.office.uec.ac.jp/campusweb/campussquare.do',
-    get_flow_execution_key(res.url),
-    shibboleth.get_cookies(),
-)
-
-campussquare.goto_syllabus_search()
-campussquare.search_syllabus(year=2021, subject='コンピュータサイエンス')
-campussquare.syllabus_detail(2021, 31, 21124124)
+if __name__ == '__main__':
+    parser = get_parser(authenticator=UECCampusSquareAuthenticator())
+    parse_args_with_handler(parser)
